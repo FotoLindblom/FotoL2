9b23037a02a2a4cb561693008f118562e84bfe9c
Rasmus Lindblom <rasmus.lindblom.04@hotmail.com>
2024-08-26 08:14:58 +0200

    fix load bug in sports

--- assets/js/sports.js
+++ assets/js/sports.js
@@ -7,30 +7,31 @@ const images = [
 ];
 
 const container = document.getElementById('image-container');
+images.forEach(image => {
+    const img = document.createElement('img');
+    img.src = `assets/images/${image}`;
+    
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('image-wrapper');
+    wrapper.appendChild(img);
 
-function addImages() {
-    images.forEach(image => {
-        const img = document.createElement('img');
-        img.src = `assets/images/${image}`;
-        
-        const wrapper = document.createElement('div');
-        wrapper.classList.add('image-wrapper');
-        wrapper.appendChild(img);
+    container.appendChild(wrapper);
+});
 
-        container.appendChild(wrapper);
-    });
-}
-
-addImages();
 
 const imgs = document.querySelectorAll('img');
-
 imgs.forEach((img) => {
-    const aspectRatio = img.naturalWidth / img.naturalHeight;
+    img.addEventListener('load', () => {
+        const aspectRatio = img.naturalWidth / img.naturalHeight;
 
-    if (aspectRatio > 1) {
-        img.classList.add('landscape');
-    } else {
-        img.classList.add('portrait');
+        if (aspectRatio > 1) {
+            img.classList.add('landscape');
+        } else {
+            img.classList.add('portrait');
+        }
+    });
+    
+    if (img.complete) {
+        img.dispatchEvent(new Event('load'));
     }
 });
